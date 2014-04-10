58a02c01e1b721338dde34e397d264f04f40209b
Mikey <Mikey@Mikey-D>
2014-04-10 17:55:13 +0200

    * Fixed progressbar * Fixed so u can select 1 option and not both of them

--- Kryptering-examensarbete/src/View/Gui.java
+++ Kryptering-examensarbete/src/View/Gui.java
@@ -26,7 +26,7 @@ public class Gui extends javax.swing.JFrame {
     private void initComponents() {
 
         fileChooser = new javax.swing.JFileChooser();
-        jTextField1 = new javax.swing.JTextField();
+        input = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jComboBox1 = new javax.swing.JComboBox();
@@ -41,7 +41,7 @@ public class Gui extends javax.swing.JFrame {
 
             setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-            jTextField1.setText("Enter string to encrypt/decrypt");
+            input.setText("Enter string to encrypt/decrypt");
 
             jButton1.setText("Encrypt");
 
@@ -49,8 +49,12 @@ public class Gui extends javax.swing.JFrame {
 
             jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "AES-128", "AES-192", "AES-256", "RSA" }));
 
-            textOption.setSelected(true);
             textOption.setText("Text");
+            textOption.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    textOptionActionPerformed(evt);
+                }
+            });
 
             fileOption.setText("File");
             fileOption.addActionListener(new java.awt.event.ActionListener() {
@@ -93,10 +97,10 @@ public class Gui extends javax.swing.JFrame {
                         .addGroup(layout.createSequentialGroup()
                             .addComponent(jLabel1)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(output, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 507, Short.MAX_VALUE)))
+                            .addComponent(input, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 507, Short.MAX_VALUE)))
                     .addContainerGap(47, Short.MAX_VALUE))
             );
             layout.setVerticalGroup(
@@ -107,7 +111,7 @@ public class Gui extends javax.swing.JFrame {
                         .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jLabel1))
                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(input, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jButton1)
@@ -136,17 +140,33 @@ public class Gui extends javax.swing.JFrame {
 
     private void fileOptionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fileOptionActionPerformed
         // TODO add your handling code here:
+        if(fileOption.isSelected())
+        {
+            textOption.setSelected(false);
+            input.setEnabled(false);
+            openFileButton.setEnabled(true);
+        }
     }//GEN-LAST:event_fileOptionActionPerformed
+
+    private void textOptionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textOptionActionPerformed
+        // TODO add your handling code here:
+        if(textOption.isSelected())
+        {
+            fileOption.setSelected(false);
+            openFileButton.setEnabled(false);
+            input.setEnabled(true);
+        }
+    }//GEN-LAST:event_textOptionActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JFileChooser fileChooser;
     private javax.swing.JCheckBox fileOption;
+    private javax.swing.JTextField input;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JProgressBar jProgressBar1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JButton openFileButton;
     private javax.swing.JTextField output;
     private javax.swing.JCheckBox textOption;
